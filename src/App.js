7ed2c2e539afbb3f2cf2a0fc1b3449c972ad7329
Shreya <diplomaticdragonnft@gmail.com>
2022-07-10 20:55:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './Dragon9.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+          Diplomatic Dragon
+        </h2>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>
+          Coming Soon!!!
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
