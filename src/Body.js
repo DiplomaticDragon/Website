bcb66a4fb7b5270cd03d6d3ec086c66f009ca41f
Shreya <diplomaticdragonnft@gmail.com>
2022-07-19 08:50:46 +0530

    Adding Discord

--- src/Body.js
+++ src/Body.js
@@ -12,43 +12,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
   return (
       <div class="container">
-          
+
         <div class="row align-items-center mt-2">
-            
-            <div class="col-sm-12 col-md-12 col-xl-6 col-lg-6 dragonStory my-5">    
+
+            <div class="col-sm-12 col-md-12 col-xl-6 col-lg-6 dragonStory my-5">
               <h1 class="welcomeDragon">
                 Welcome to the world of Dragons
-              </h1>           
+              </h1>
               <h2 class="dragonStoryLine my-5">
-                Generally dragons are very fierce, but we, NFT Dragons are peaceful, cute and want to live with human on earth.
-              </h2>   
-              
+                Generally, dragons are very fierce, but we, the NFT Dragons are peaceful, cute and want to live with humans on earth.
+              </h2>
+
               <h2 class="dragonStoryLine my-2">
-                We are going to build our own community. 
-                Here is the first member of <b>Diplomatic Dragon</b> community. 
-              </h2>    
-                        
+                We are going to build our community.
+                Here is the first member of the <b>Diplomatic Dragon</b> community.
+              </h2>
+
               <h2 class="comingSoon my-5">
                 Diplomatic Dragons are on their way to Earth!!!
               </h2>
-              
+
             </div>
-            
-            <div class="col-sm-12 col-md-12 col-xl-6 col-lg-6">              
+
+            <div class="col-sm-12 col-md-12 col-xl-6 col-lg-6">
               <img src={dragonImage} className="App-dragonImage" alt="logo" />
             </div>
-            
+
         </div>
-        
+
           <div class="row align-items-center mx-5">
             <h2 class="socialLine mb-4">
-              Follow Dragons on social and lucky 100 people will get a free dragon before public launch.
+              Follow Dragons on social media platforms, and a lucky 100 people will get a free dragon before public launch.
             </h2>
           </div>
-          
+
           <div class="row align-items-center pb-5">
               <a href="https://opensea.io/collection/diplomaticdragon">
-                <img src={openSeaDark} class="opensea" alt="logo"/>                
+                <img src={openSeaDark} class="opensea" alt="logo"/>
               </a>
           </div>
       </div>
--- src/Header.js
+++ src/Header.js
@@ -5,15 +5,16 @@ import './Header.css';
 import './App.css';
 import {
   faTwitter,
-  faInstagram
+  faInstagram,
+  faDiscord
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
-    
+
         <div class="d-flex justify-content-between align-items-center w-full md:p-6 p-3">
-          
+
           <div id="logoDiv" class="d-flex align-items-center">
           <a href="/" class="text-inherit">
             <img src={logo} className="logo justify-content-start d-none d-md-block d-xl-block d-lg-block" alt="logo"/>
@@ -22,22 +23,27 @@ function App() {
               Diplomatic Dragon
             </a>
           </div>
-          
-          <div class="d-flex md:flex-wrap justify-content-evenly align-items-center md:text-2xl text-l">    
+
+          <div class="d-flex md:flex-wrap justify-content-evenly align-items-center md:text-2xl text-l">
           <div class="auth justify-content-center align-items-center d-none d-sm-block">
-            <a href="https://twitter.com/DiplomatDragon" 
+            <a href="https://twitter.com/DiplomatDragon"
               className="text-inherit twitter social twitterlocal mx-4">
             <FontAwesomeIcon icon={faTwitter} size="2x" />
             </a>
-          
+
             <a href="https://www.instagram.com/diplomaticdragon/"
                className="text-inherit instagram social instagramlocal mx-4">
             <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
+
+            <a href="https://discord.com/invite/MShWFeYz/"
+                 className="text-inherit instagram social instagramlocal mx-4">
+            <FontAwesomeIcon icon={faDiscord} size="2x" />
+            </a>
             </div>
           </div>
 
-        </div>        
+        </div>
   );
 }
 
